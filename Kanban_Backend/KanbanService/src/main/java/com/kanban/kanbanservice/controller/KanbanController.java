01fec035aafbac9530e024c24f4e412234da588c
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-12-30 07:14:26 +0000

    added KanbanController.java class

---
+++ Kanban_Backend/KanbanService/src/main/java/com/kanban/kanbanservice/controller/KanbanController.java
@@ -0,0 +1,9 @@
+package com.kanban.kanbanservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kanban")
+public class KanbanController {
+}
