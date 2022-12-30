a52c2a2e474d3201d3f6eb34f38c70c0b8faaa6d
M V K <krishna.mugala9@gmail.com>
2022-12-30 12:34:27 +0530

    added a custom method in repository class

--- Kanban_Backend/NotificationService/src/main/java/com/kanban/notificationservice/repository/NotificationRepository.java
+++ Kanban_Backend/NotificationService/src/main/java/com/kanban/notificationservice/repository/NotificationRepository.java
@@ -1,4 +1,11 @@
 package com.kanban.notificationservice.repository;
 
-public interface NotificationRepository {
+import com.kanban.notificationservice.domain.Notification;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends MongoRepository<Notification, String> {
+
+    Notification findByEmail(String email);
 }
