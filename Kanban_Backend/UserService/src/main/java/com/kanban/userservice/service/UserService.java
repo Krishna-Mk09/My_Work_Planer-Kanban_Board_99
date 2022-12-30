3bf05ba601c563e584f3b42a0631c62342dce359
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-12-30 12:29:54 +0530

    added updateUser method

--- Kanban_Backend/UserService/src/main/java/com/kanban/userservice/service/UserService.java
+++ Kanban_Backend/UserService/src/main/java/com/kanban/userservice/service/UserService.java
@@ -4,4 +4,8 @@ import com.kanban.userservice.domain.User;
 
 public interface UserService {
 	User registorUser(User user);
+
+	User updateUser(User user);
+
+//	User loginUser(String email, String password);
 }
