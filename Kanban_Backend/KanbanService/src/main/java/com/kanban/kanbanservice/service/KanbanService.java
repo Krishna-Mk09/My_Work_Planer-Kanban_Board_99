94bbc76cb024b74d31a528653fe914df674e076b
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-12-30 07:10:38 +0000

    added java docs for KanbanService interface

--- Kanban_Backend/KanbanService/src/main/java/com/kanban/kanbanservice/service/KanbanService.java
+++ Kanban_Backend/KanbanService/src/main/java/com/kanban/kanbanservice/service/KanbanService.java
@@ -3,12 +3,31 @@ package com.kanban.kanbanservice.service;
 import com.kanban.kanbanservice.domain.Kanban;
 
 public interface KanbanService {
+    /**
+     * Get the Kanban Board by email id
+     * @param email The email if for the user
+     * @return The Kanban Board for that user
+     */
     Kanban getKanbanByEmail(String email);
 
+    /**
+     * Save the Kanban Board
+     * @param kanban The Kanban Board to save
+     * @return The saved Kanban Board
+     */
     Kanban saveKanban(Kanban kanban);
 
+    /**
+     * Update the Kanban Board
+     * @param kanban The Kanban Board to update
+     * @return The updated Kanban Board
+     */
     Kanban updateKanbanBoard(Kanban kanban);
 
+    /**
+     * Delete the Kanban Board by email id
+     * @param email The email id for the user
+     */
     void deleteKanbanBoardByEmail(String email);
 
 
