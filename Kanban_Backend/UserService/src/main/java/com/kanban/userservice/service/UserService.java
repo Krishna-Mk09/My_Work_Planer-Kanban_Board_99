59d825dc4a0ddf33431502b380fedf32d5c698f7
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-12-30 12:40:35 +0530

    added documents of methods

--- Kanban_Backend/UserService/src/main/java/com/kanban/userservice/service/UserService.java
+++ Kanban_Backend/UserService/src/main/java/com/kanban/userservice/service/UserService.java
@@ -3,11 +3,35 @@ package com.kanban.userservice.service;
 import com.kanban.userservice.domain.User;
 
 public interface UserService {
+
+	/**
+	 * This method is used to save the user in the database and return the saved user.
+	 *
+	 * @param user This is the user object which is to be saved in the database.
+	 * @return User
+	 */
 	User registorUser(User user);
 
+	/**
+	 * This method is used to update the user in the database and return the updated user.
+	 *
+	 * @param user This is the user object which is to be updated in the database.
+	 * @return User
+	 */
 	User updateUser(User user);
 
-	User deleteUser(String email);
+	/**
+	 * This method is used to delete the user from the database and return the deleted user.
+	 *
+	 * @param email This is the email of the user which is to be deleted from the database.
+	 */
+	void deleteUser(String email);
 
+	/**
+	 * This method is used to log in the user and return the user.
+	 *
+	 * @param email and password This is the email and password of the user which is to be logged in.
+	 * @return User
+	 */
 	User loginUser(String email, String password);
 }
