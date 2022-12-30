4f95e7d5d1b51d8dc627d5ce72e4f064b97550c2
M V K <krishna.mugala9@gmail.com>
2022-12-30 13:11:21 +0530

    added methods inside notification service

--- Kanban_Backend/NotificationService/src/main/java/com/kanban/notificationservice/service/NotificationService.java
+++ Kanban_Backend/NotificationService/src/main/java/com/kanban/notificationservice/service/NotificationService.java
@@ -1,4 +1,11 @@
 package com.kanban.notificationservice.service;
 
+import com.kanban.notificationservice.domain.Notification;
+
 public interface NotificationService {
+    Notification saveNotification(Notification notification);
+
+    Notification getByEmail(String email);
+
+    Notification updateNotification(Notification notification);
 }
