1e94416e6ffd9fa1c5cf0cd2d42f44cb9f939b2c
M V K <krishna.mugala9@gmail.com>
2022-12-30 16:07:57 +0530

    added notification controller class

---
+++ Kanban_Backend/NotificationService/src/main/java/com/kanban/notificationservice/controller/NotificationController.java
@@ -0,0 +1,12 @@
+/*
+ * Author Name : M.Krishna.
+ * Date: 30-12-2022
+ * Created With: IntelliJ IDEA Community Edition
+ *
+ */
+
+
+package com.kanban.notificationservice.controller;
+
+public class NotificationController {
+}
