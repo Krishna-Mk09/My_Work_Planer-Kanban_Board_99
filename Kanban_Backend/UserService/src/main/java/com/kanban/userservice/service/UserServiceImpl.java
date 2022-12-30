29318fceae9808410d323945f37ed80c98e30d3b
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-12-30 12:43:45 +0530

    implemented UserServiceImpl

--- Kanban_Backend/UserService/src/main/java/com/kanban/userservice/service/UserServiceImpl.java
+++ Kanban_Backend/UserService/src/main/java/com/kanban/userservice/service/UserServiceImpl.java
@@ -7,5 +7,54 @@
  */
 package com.kanban.userservice.service;
 
-public class UserServiceImpl {
+import com.kanban.userservice.domain.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	/**
+	 * This method is used to save the user in the database and return the saved user.
+	 *
+	 * @param user This is the user object which is to be saved in the database.
+	 * @return User
+	 */
+	@Override
+	public User registorUser(User user) {
+		return null;
+	}
+
+	/**
+	 * This method is used to update the user in the database and return the updated user.
+	 *
+	 * @param user This is the user object which is to be updated in the database.
+	 * @return User
+	 */
+	@Override
+	public User updateUser(User user) {
+		return null;
+	}
+
+	/**
+	 * This method is used to delete the user from the database and return the deleted user.
+	 *
+	 * @param email This is the email of the user which is to be deleted from the database.
+	 */
+	@Override
+	public void deleteUser(String email) {
+
+	}
+
+	/**
+	 * This method is used to log in the user and return the user.
+	 *
+	 * @param email    and password This is the email and password of the user which is to be logged in.
+	 * @param password
+	 * @return User
+	 */
+	@Override
+	public User loginUser(String email, String password) {
+		return null;
+	}
 }
+
