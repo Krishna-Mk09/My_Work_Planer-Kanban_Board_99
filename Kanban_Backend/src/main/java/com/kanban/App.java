8e60c0468b5556b1c124c34c855e450496aa1251
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-12-30 11:28:51 +0530

    initial commit

---
+++ Kanban_Backend/src/main/java/com/kanban/App.java
@@ -0,0 +1,13 @@
+package com.kanban;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
