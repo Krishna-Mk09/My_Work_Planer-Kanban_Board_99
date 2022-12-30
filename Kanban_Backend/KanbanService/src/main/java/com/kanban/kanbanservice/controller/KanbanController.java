8a0238ac7f9e98379b98490cf1409861e45b6269
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-12-30 09:46:08 +0000

    added Kanban controller api methods

--- Kanban_Backend/KanbanService/src/main/java/com/kanban/kanbanservice/controller/KanbanController.java
+++ Kanban_Backend/KanbanService/src/main/java/com/kanban/kanbanservice/controller/KanbanController.java
@@ -1,9 +1,40 @@
 package com.kanban.kanbanservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.kanban.kanbanservice.domain.Kanban;
+import com.kanban.kanbanservice.service.KanbanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/kanban")
 public class KanbanController {
+    private final KanbanService kanbanService;
+
+    @Autowired
+    public KanbanController(KanbanService kanbanService) {
+        this.kanbanService = kanbanService;
+    }
+
+    @PostMapping("/save-kanban")
+    public ResponseEntity<?> saveKanban(@RequestBody Kanban kanban) {
+        return new ResponseEntity<>(this.kanbanService.saveKanban(kanban), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/get-kanban")
+    public ResponseEntity<?> getKanban(@RequestBody String email) {
+        return new ResponseEntity<>(this.kanbanService.getKanbanByEmail(email), HttpStatus.OK);
+    }
+
+    @PutMapping("/update-kanban")
+    public ResponseEntity<?> updateKanban(@RequestBody Kanban kanban) {
+        return new ResponseEntity<>(this.kanbanService.updateKanbanBoard(kanban), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete-kanban")
+    public ResponseEntity<?> deleteKanban(@RequestBody String email) {
+        this.kanbanService.deleteKanbanBoardByEmail(email);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
