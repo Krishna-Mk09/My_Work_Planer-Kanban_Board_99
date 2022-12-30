9c746d136f730d66de9c8773e5735becb3a888b7
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-12-30 12:23:04 +0530

    extended UserRepository interface to JpaRepository

--- Kanban_Backend/UserService/src/main/java/com/kanban/userservice/repository/UserRepository.java
+++ Kanban_Backend/UserService/src/main/java/com/kanban/userservice/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package com.kanban.userservice.repository;
 
+import com.kanban.userservice.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, String> {
+
 }
