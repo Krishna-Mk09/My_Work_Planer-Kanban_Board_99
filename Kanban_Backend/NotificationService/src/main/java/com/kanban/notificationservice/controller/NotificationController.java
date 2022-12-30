c1a96492edd44b4f1d8ebb2a56e361bc7a505a4a
M V K <krishna.mugala9@gmail.com>
2022-12-30 16:10:16 +0530

    called the notification service in controller class

--- Kanban_Backend/NotificationService/src/main/java/com/kanban/notificationservice/controller/NotificationController.java
+++ Kanban_Backend/NotificationService/src/main/java/com/kanban/notificationservice/controller/NotificationController.java
@@ -8,5 +8,18 @@
 
 package com.kanban.notificationservice.controller;
 
+import com.kanban.notificationservice.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("notification")
 public class NotificationController {
+    NotificationService notificationService;
+
+    @Autowired
+    public NotificationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
 }
