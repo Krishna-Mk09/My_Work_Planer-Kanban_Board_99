c5364547b85e14fd685679422745f7944d601af6
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-12-30 12:18:02 +0530

    added getter and setter methods

--- Kanban_Backend/UserService/src/main/java/com/kanban/userservice/domain/User.java
+++ Kanban_Backend/UserService/src/main/java/com/kanban/userservice/domain/User.java
@@ -31,4 +31,44 @@ public class User {
 		this.password = password;
 		this.mobileNumber = mobileNumber;
 	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public long getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(long mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
 }
