0a1c4369eb24a6065fdaa70cda7e18977cf20260
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-12-30 11:32:20 +0530

    added kanban service

---
+++ Kanban_Backend/KanbanService/src/main/java/com/kanban/kanbanservice/KanbanServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kanban.kanbanservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanServiceApplication.class, args);
+	}
+
+}
---
+++ Kanban_Backend/KanbanService/src/test/java/com/kanban/kanbanservice/KanbanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanban.kanbanservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
