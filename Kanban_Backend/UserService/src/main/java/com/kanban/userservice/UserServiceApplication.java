ba4a141034d2dbfbdc82d13eb428d9c8fe8aa5b1
M V K <krishna.mugala9@gmail.com>
2023-01-02 11:09:09 +0530

    added  eureka client dependencies in user pom

--- Kanban_Backend/UserService/src/main/java/com/kanban/userservice/UserServiceApplication.java
+++ Kanban_Backend/UserService/src/main/java/com/kanban/userservice/UserServiceApplication.java
@@ -4,11 +4,13 @@ import com.kanban.userservice.filter.JwtFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
