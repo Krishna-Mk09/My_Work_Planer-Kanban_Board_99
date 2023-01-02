cdd7bd11b5ffb664088020c42aebc347ef4c609d
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2023-01-02 17:43:49 +0530

    added rabbitmq

--- Kanban_Backend/KanbanService/src/main/java/com/kanban/kanbanservice/configuration/Producer.java
+++ Kanban_Backend/KanbanService/src/main/java/com/kanban/kanbanservice/configuration/Producer.java
@@ -6,16 +6,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Producer {
-    private final RabbitTemplate rabbitTemplate;
-    private final DirectExchange directExchange;
+	private final RabbitTemplate rabbitTemplate;
+	private final DirectExchange directExchange;
 
-    public Producer(RabbitTemplate rabbitTemplate, DirectExchange directExchange) {
-        this.rabbitTemplate = rabbitTemplate;
-        this.directExchange = directExchange;
-    }
+	public Producer(RabbitTemplate rabbitTemplate, DirectExchange directExchange) {
+		this.rabbitTemplate = rabbitTemplate;
+		this.directExchange = directExchange;
+	}
 
-    public void sendMessage(MessageDTO messageDTO) {
-        rabbitTemplate.convertAndSend(directExchange.getName(), "messageKey", messageDTO);
-    }
+	public void sendMessage(MessageDTO messageDTO) {
+		rabbitTemplate.convertAndSend(directExchange.getName(), "messageKey", messageDTO);
+	}
 
 }
--- Kanban_Backend/KanbanService/src/main/java/com/kanban/kanbanservice/service/KanbanServiceImpl.java
+++ Kanban_Backend/KanbanService/src/main/java/com/kanban/kanbanservice/service/KanbanServiceImpl.java
@@ -1,60 +1,84 @@
 package com.kanban.kanbanservice.service;
 
+import com.kanban.kanbanservice.configuration.MessageDTO;
+import com.kanban.kanbanservice.configuration.Producer;
 import com.kanban.kanbanservice.domain.Kanban;
 import com.kanban.kanbanservice.repository.KanbanRepository;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KanbanServiceImpl implements KanbanService {
-    private final KanbanRepository kanbanRepository;
+	private final KanbanRepository kanbanRepository;
 
-    @Autowired
-    public KanbanServiceImpl(KanbanRepository kanbanRepository) {
-        this.kanbanRepository = kanbanRepository;
-    }
+	private final RabbitTemplate rabbitTemplate;
+	private final DirectExchange directExchange;
 
-    /**
-     * Get the Kanban Board by email id
-     * @param email The email if for the user
-     * @return The Kanban Board for that user
-     */
-    @Override
-    public Kanban getKanbanByEmail(String email) {
-        return this.kanbanRepository.findKanbanByEmail(email);
-    }
+	@Autowired
+	Producer producer;
 
-    /**
-     * Save the Kanban Board
-     * @param kanban The Kanban Board to save
-     * @return The saved Kanban Board
-     */
-    @Override
-    public Kanban saveKanban(Kanban kanban) {
-        return this.kanbanRepository.save(kanban);
-    }
+	@Autowired
+	public KanbanServiceImpl(KanbanRepository kanbanRepository, RabbitTemplate rabbitTemplate, DirectExchange directExchange) {
+		this.kanbanRepository = kanbanRepository;
+		this.rabbitTemplate = rabbitTemplate;
+		this.directExchange = directExchange;
+	}
 
-    /**
-     * Update the Kanban Board
-     * @param kanban The Kanban Board to update
-     * @return The updated Kanban Board
-     */
-    @Override
-    public Kanban updateKanbanBoard(Kanban kanban) {
+	/**
+	 * Get the Kanban Board by email id
+	 *
+	 * @param email The email if for the user
+	 * @return The Kanban Board for that user
+	 */
+	@Override
+	public Kanban getKanbanByEmail(String email) {
+		return this.kanbanRepository.findKanbanByEmail(email);
+	}
+
+	/**
+	 * Save the Kanban Board
+	 *
+	 * @param kanban The Kanban Board to save
+	 * @return The saved Kanban Board
+	 */
+	@Override
+	public Kanban saveKanban(Kanban kanban) {
+		return this.kanbanRepository.save(kanban);
+	}
+
+	/**
+	 * Update the Kanban Board
+	 *
+	 * @param kanban The Kanban Board to update
+	 * @return The updated Kanban Board
+	 */
+	@Override
+	public Kanban updateKanbanBoard(Kanban kanban) {
 //        Kanban kanbanByEmail = this.getKanbanByEmail(kanban.getEmail());
 //        if (kanbanByEmail != null) {
 //            kanbanByEmail.setBoards(kanban.getBoards());
 //            return this.kanbanRepository.save(kanbanByEmail);
 //        }
-        return this.kanbanRepository.save(kanban);
-    }
+		return this.kanbanRepository.save(kanban);
+	}
 
-    /**
-     * Delete the Kanban Board by email id
-     * @param email The email id for the user
-     */
+	/**
+	 * Delete the Kanban Board by email id
+	 *
+	 * @param email The email id for the user
+	 */
+	@Override
+	public void deleteKanbanBoardByEmail(String email) {
+		this.kanbanRepository.deleteById(email);
+	}
+
     @Override
-    public void deleteKanbanBoardByEmail(String email) {
-        this.kanbanRepository.deleteById(email);
-    }
+    public MessageDTO sendMessage(MessageDTO messageDTO) {
+//        if (this.kanbanRepository.existsById(messageDTO.getEmail())) {
+            producer.sendMessage(messageDTO);
+//        }
+		return messageDTO;
+	}
 }
