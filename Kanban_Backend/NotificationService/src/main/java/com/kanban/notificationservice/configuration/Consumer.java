1c7939b332d51d0ba26201b653e2e537177d5e2e
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2023-01-02 17:43:03 +0530

    created Consumer class

---
+++ Kanban_Backend/NotificationService/src/main/java/com/kanban/notificationservice/configuration/Consumer.java
@@ -0,0 +1,33 @@
+package com.kanban.notificationservice.configuration;
+
+import com.kanban.notificationservice.domain.Notification;
+import com.kanban.notificationservice.service.NotificationService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class Consumer {
+
+	private final NotificationService notificationService;
+
+	@Autowired
+	public Consumer(NotificationService notificationService) {
+		this.notificationService = notificationService;
+	}
+
+	@RabbitListener(queues = "messageQueue")
+	public void getData(MessageDTO messageDTO) {
+		Notification notificationByEmail = this.notificationService.getByEmail(messageDTO.getEmail());
+		List<String> message = notificationByEmail.getMessage();
+		if (message == null) {
+			message = new ArrayList<>();
+		}
+		message.add(messageDTO.getMessage());
+		notificationByEmail.setMessage(message);
+		this.notificationService.saveNotification(notificationByEmail);
+	}
+}
