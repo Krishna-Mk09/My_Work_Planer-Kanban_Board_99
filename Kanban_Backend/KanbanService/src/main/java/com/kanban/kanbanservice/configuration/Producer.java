deeb1a0c2d1684e8fffc49a1e4ccf0bbf55276f5
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2023-01-02 14:47:53 +0530

    added Producer.java class

---
+++ Kanban_Backend/KanbanService/src/main/java/com/kanban/kanbanservice/configuration/Producer.java
@@ -0,0 +1,21 @@
+package com.kanban.kanbanservice.configuration;
+
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Producer {
+    private final RabbitTemplate rabbitTemplate;
+    private final DirectExchange directExchange;
+
+    public Producer(RabbitTemplate rabbitTemplate, DirectExchange directExchange) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.directExchange = directExchange;
+    }
+
+    public void sendMessage(MessageDTO messageDTO) {
+        rabbitTemplate.convertAndSend(directExchange.getName(), "messageKey", messageDTO);
+    }
+
+}
