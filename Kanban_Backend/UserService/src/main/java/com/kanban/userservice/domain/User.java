278dc6b02ec6814c4671a726579cd4ff567c5aef
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2023-01-03 00:49:51 +0530

    replaced default methods with lombok annotations

--- Kanban_Backend/UserService/src/main/java/com/kanban/userservice/domain/User.java
+++ Kanban_Backend/UserService/src/main/java/com/kanban/userservice/domain/User.java
@@ -7,79 +7,22 @@
  */
 package com.kanban.userservice.domain;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.PrimaryKeyJoinColumn;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class User {
-	String firstName;
-	String lastName;
+	private String firstName;
+	private String lastName;
 	@Id
-//	@PrimaryKeyJoinColumn
-	String email;
-	String password;
-	long mobileNumber;
-
-	public User() {
-	}
-
-	public User(String firstName, String lastName, String email, String password, long mobileNumber) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-		this.password = password;
-		this.mobileNumber = mobileNumber;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public long getMobileNumber() {
-		return mobileNumber;
-	}
-
-	public void setMobileNumber(long mobileNumber) {
-		this.mobileNumber = mobileNumber;
-	}
-
-	@Override
-	public String toString() {
-		return "User{" +
-			"firstName='" + firstName + '\'' +
-			", lastName='" + lastName + '\'' +
-			", email='" + email + '\'' +
-			", password='" + password + '\'' +
-			", mobileNumber=" + mobileNumber +
-			'}';
-	}
+	private String email;
+	private String password;
+	private long mobileNumber;
 }
