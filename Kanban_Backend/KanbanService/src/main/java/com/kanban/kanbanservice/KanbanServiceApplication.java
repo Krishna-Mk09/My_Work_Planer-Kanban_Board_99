379a59d3145bf8748238c4ef604b56100983bf01
M V K <krishna.mugala9@gmail.com>
2023-01-02 11:19:57 +0530

    registered kanban service to eureka service

--- Kanban_Backend/KanbanService/src/main/java/com/kanban/kanbanservice/KanbanServiceApplication.java
+++ Kanban_Backend/KanbanService/src/main/java/com/kanban/kanbanservice/KanbanServiceApplication.java
@@ -2,9 +2,10 @@ package com.kanban.kanbanservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-
+@EnableEurekaClient
 public class KanbanServiceApplication {
 
 	public static void main(String[] args) {
