11a92fa384ec223ccc8f622fbe93090897facbe5
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2023-01-03 14:32:38 +0530

    added addMemberToBoardByEmail method

--- Kanban_Backend/KanbanService/src/main/java/com/kanban/kanbanservice/repository/KanbanRepository.java
+++ Kanban_Backend/KanbanService/src/main/java/com/kanban/kanbanservice/repository/KanbanRepository.java
@@ -1,5 +1,6 @@
 package com.kanban.kanbanservice.repository;
 
+import com.kanban.kanbanservice.domain.Board;
 import com.kanban.kanbanservice.domain.Kanban;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +8,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface KanbanRepository extends MongoRepository<Kanban, String> {
     Kanban findKanbanByEmail(String email);
+
+    Kanban findByBoardsAndAndEmail();
+
+
+
 }
