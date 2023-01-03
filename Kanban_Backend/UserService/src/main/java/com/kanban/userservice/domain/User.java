6534ca63c5adbad0505315f4f754457137c7065a
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2023-01-03 13:57:38 +0530

    updated lambok annotations

--- Kanban_Backend/UserService/src/main/java/com/kanban/userservice/domain/User.java
+++ Kanban_Backend/UserService/src/main/java/com/kanban/userservice/domain/User.java
@@ -13,11 +13,9 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
-@ToString
 public class User {
 	private String firstName;
 	private String lastName;
