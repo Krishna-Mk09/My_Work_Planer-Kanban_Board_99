c1ff43941e906a8768fcee2b7e44c59d602390be
M V K <krishna.mugala9@gmail.com>
2023-01-03 16:01:38 +0530

    added @config annotation in main class

--- Kanban_Backend/ApiGatewayService/src/main/java/com/kanban/apigatewayservice/ApiGatewayServiceApplication.java
+++ Kanban_Backend/ApiGatewayService/src/main/java/com/kanban/apigatewayservice/ApiGatewayServiceApplication.java
@@ -3,9 +3,11 @@ package com.kanban.apigatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableEurekaClient
+@Configuration
 public class ApiGatewayServiceApplication {
 
 	public static void main(String[] args) {
