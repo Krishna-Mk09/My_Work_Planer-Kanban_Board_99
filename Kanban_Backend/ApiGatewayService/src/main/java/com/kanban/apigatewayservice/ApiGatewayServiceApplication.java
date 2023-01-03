ea084578302b62d4616be6f4fac1d91986b1da26
M V K <krishna.mugala9@gmail.com>
2023-01-03 16:07:32 +0530

    added path of each service to the gate-way

--- Kanban_Backend/ApiGatewayService/src/main/java/com/kanban/apigatewayservice/ApiGatewayServiceApplication.java
+++ Kanban_Backend/ApiGatewayService/src/main/java/com/kanban/apigatewayservice/ApiGatewayServiceApplication.java
@@ -2,7 +2,10 @@ package com.kanban.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
@@ -10,8 +13,24 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ApiGatewayServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayServiceApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+
+				.route(p -> p.path("/kanban/**")
+						.uri("http://kanban-service:8082/")) // use the name of the application in the uri
+
+                .route(p -> p.path("/user/**")
+						.uri("http://user-service:8081/"))
+
+				.route(p -> p.path("/notification/**")
+						.uri("http://notification-service:8083/"))
+
+				.build();
+    }
 
 }
