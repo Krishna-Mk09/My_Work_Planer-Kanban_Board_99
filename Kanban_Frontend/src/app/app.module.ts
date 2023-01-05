edcfe73bd4aeeb315bcca3d6d561ad7f1a1f501b
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2023-01-05 14:01:15 +0530

    added login component

--- Kanban_Frontend/src/app/app.module.ts
+++ Kanban_Frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
