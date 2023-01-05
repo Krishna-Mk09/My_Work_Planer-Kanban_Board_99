836688adaf40c14c089c0029ee2cec2d81ad7283
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2023-01-05 22:05:07 +0530

    updated confirm password validators

--- Kanban_Frontend/src/app/components/register/register.component.ts
+++ Kanban_Frontend/src/app/components/register/register.component.ts
@@ -15,7 +15,7 @@ export class RegisterComponent {
     lastName: [null, [Validators.required, Validators.minLength(2), Validators.maxLength(20), Validators.pattern('[a-zA-Z ]*')]],
     email: [null, [Validators.required, Validators.minLength(8), Validators.maxLength(25), Validators.pattern(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)]],
     password: [null, [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/)]],
-    confirmPassword: [null, [Validators.required, Validators.minLength(4), Validators.maxLength(6), Validators.pattern(/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/)]],
+    confirmPassword: [null, [Validators.required, Validators.minLength(8), Validators.maxLength(12), Validators.pattern(/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/)]],
     mobileNumber: [null, [Validators.required, Validators.minLength(10), Validators.maxLength(12), Validators.pattern(/^\+[1-9]{2}-[0-9]{3}-[0-9]{3}-[0-9]{4}$/)]],
   });
 
@@ -33,7 +33,6 @@ export class RegisterComponent {
     this.authentication.registerUser(user).subscribe({
       next: () => {
         this.snackBar.open("Registered Successfully!", "Close");
-        this.userForm.reset();
       },
       error: (err) => {
         if (err.status === 409) {
