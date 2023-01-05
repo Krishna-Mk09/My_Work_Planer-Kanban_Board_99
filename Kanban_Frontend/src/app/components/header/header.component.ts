39e6d1908f03149d5498615d379b2a2804f416c8
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2023-01-05 22:06:26 +0530

    added logout in header

--- Kanban_Frontend/src/app/components/header/header.component.ts
+++ Kanban_Frontend/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from "../../services/authentication.service";
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authentication: AuthenticationService) {
+  }
 
   ngOnInit(): void {
   }
 
+  logout() {
+    this.authentication.logoutUser();
+  }
 }
