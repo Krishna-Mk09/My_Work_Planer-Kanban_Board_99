1376f620b46f30f71b1e33837bf27532dd732055
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2023-01-05 22:07:36 +0530

    added ngOnInit to fetch user profile if token is already there

--- Kanban_Frontend/src/app/app.component.ts
+++ Kanban_Frontend/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from "./services/authentication.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Kanban_Frontend';
+
+  constructor(private authentication: AuthenticationService) {
+  }
+
+  ngOnInit(): void {
+    if (localStorage.getItem('token') != null) {
+      this.authentication.getUserProfile();
+    }
+  }
 }
