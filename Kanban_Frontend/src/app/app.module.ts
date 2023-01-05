7753d4fc7eac41ae221915f63dc323e56a5d439f
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2023-01-05 14:02:10 +0530

    added register component

--- Kanban_Frontend/src/app/app.module.ts
+++ Kanban_Frontend/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
