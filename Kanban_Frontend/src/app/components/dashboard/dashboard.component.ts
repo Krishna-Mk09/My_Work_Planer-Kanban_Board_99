bed9927083b17a77b55a78c5c4198d03afdc86ca
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2023-01-06 11:21:04 +0530

    added ngOnInit to fetch kanban data

--- Kanban_Frontend/src/app/components/dashboard/dashboard.component.ts
+++ Kanban_Frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,9 @@
 import {Component, OnInit} from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {Observable} from 'rxjs';
+import {map, shareReplay} from 'rxjs/operators';
+import {KanbanService} from "../../services/kanban.service";
+import {Kanban} from "../../model/kanban/Kanban";
 
 @Component({
   selector: 'app-dashboard',
@@ -7,10 +12,20 @@ import {Component, OnInit} from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() {
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  currentUserKanban?: Kanban;
+
+  constructor(private breakpointObserver: BreakpointObserver, private kanbanService: KanbanService) {
   }
 
   ngOnInit(): void {
+    this.kanbanService.getKanbanByEmail();
+    this.currentUserKanban = this.kanbanService.currentUserKanban;
   }
 
 }
