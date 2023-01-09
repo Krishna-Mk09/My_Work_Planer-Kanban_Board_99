2d3d689e84d981974fbfa0f44cd4ff6df89793ed
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2023-01-09 10:35:14 +0530

    added getAllEmailsInBoard method in kanban

--- Kanban_Backend/KanbanService/src/main/java/com/kanban/kanbanservice/service/KanbanService.java
+++ Kanban_Backend/KanbanService/src/main/java/com/kanban/kanbanservice/service/KanbanService.java
@@ -1,36 +1,43 @@
 package com.kanban.kanbanservice.service;
 
-import com.kanban.kanbanservice.configuration.MessageDTO;
 import com.kanban.kanbanservice.domain.Kanban;
+
+import java.util.List;
 
 public interface
 KanbanService {
-    /**
-     * Get the Kanban Board by email id
-     * @param email The email if for the user
-     * @return The Kanban Board for that user
-     */
-    Kanban getKanbanByEmail(String email);
+	/**
+	 * Get the Kanban Board by email id
+	 *
+	 * @param email The email if for the user
+	 * @return The Kanban Board for that user
+	 */
+	Kanban getKanbanByEmail(String email);
 
-    /**
-     * Save the Kanban Board
-     * @param kanban The Kanban Board to save
-     * @return The saved Kanban Board
-     */
-    Kanban saveKanban(Kanban kanban);
+	/**
+	 * Save the Kanban Board
+	 *
+	 * @param kanban The Kanban Board to save
+	 * @return The saved Kanban Board
+	 */
+	Kanban saveKanban(Kanban kanban);
 
-    /**
-     * Update the Kanban Board
-     * @param kanban The Kanban Board to update
-     * @return The updated Kanban Board
-     */
-    Kanban updateKanban(Kanban kanban);
+	/**
+	 * Update the Kanban Board
+	 *
+	 * @param kanban The Kanban Board to update
+	 * @return The updated Kanban Board
+	 */
+	Kanban updateKanban(Kanban kanban);
 
-    /**
-     * Delete the Kanban Board by email id
-     * @param email The email id for the user
-     */
-    void deleteKanbanByEmail(String email);
+	/**
+	 * Delete the Kanban Board by email id
+	 *
+	 * @param email The email id for the user
+	 */
+	void deleteKanbanByEmail(String email);
 
-    String addMemberToBoardByEmail(Kanban kanban, String email);
+	String addMemberToBoardByEmail(Kanban kanban, String email);
+
+	List<String> getAllEmailsInBoard(String email, String boardName);
 }
