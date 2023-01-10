f929ab0b486739d5d00ead9ac64f50defddc0af6
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2023-01-10 16:33:41 +0530

    updated header items based on login condition

--- Kanban_Frontend/src/app/components/header/header.component.ts
+++ Kanban_Frontend/src/app/components/header/header.component.ts
@@ -1,28 +1,33 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthenticationService} from "../../services/authentication.service";
+import {User} from "../../model/user/User";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: 'app-header', templateUrl: './header.component.html', styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+
+  aditya: string = "hidden";
+  currentUser?: User;
+  isLoggedIn: boolean = false;
 
   constructor(private authentication: AuthenticationService) {
   }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.currentUser = this.authentication.currentUser;
+      this.isLoggedIn = this.authentication.isLoggedIn;
+    }, 1000)
   }
 
-  aditya:string = "hidden";
-
-  changeColour(){
+  changeColour() {
     return this.aditya;
   }
 
-  changeColour1(){
-    if(this.aditya === "hidden"){
-      this.aditya= "visible";
+  changeColour1() {
+    if (this.aditya === "hidden") {
+      this.aditya = "visible";
     } else {
       this.aditya = "hidden";
     }
@@ -30,5 +35,6 @@ export class HeaderComponent implements OnInit {
 
   logout() {
     this.authentication.logoutUser();
+    window.location.reload();
   }
 }
