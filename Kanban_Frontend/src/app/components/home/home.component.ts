87f79cdee2e7b4ac17a146f3992f41c52e8ca68f
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2023-01-10 16:34:47 +0530

    updated home component based on login condition

--- Kanban_Frontend/src/app/components/home/home.component.ts
+++ Kanban_Frontend/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from "../../services/authentication.service";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  isLoggedIn: boolean = false;
+
+  constructor(private authentication: AuthenticationService) {
+  }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.isLoggedIn = this.authentication.isLoggedIn;
+    }, 1000)
   }
 
 }
