e01cb35cd929fc5d876f81b86c97a0ab7abc46b0
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2023-01-15 22:37:01 +0530

    removed notification popup

--- Kanban_Frontend/src/app/components/header/header.component.ts
+++ Kanban_Frontend/src/app/components/header/header.component.ts
@@ -1,13 +1,9 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthenticationService} from "../../services/authentication.service";
 import {User} from "../../model/user/User";
-import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from "@angular/material/dialog";
 import {NotificationService} from "../../services/notification.service";
 import {Notification} from "../../model/notification/Notification";
 
-export interface MessageData {
-  messages: string[];
-}
 
 @Component({
   selector: 'app-header', templateUrl: './header.component.html', styleUrls: ['./header.component.css']
@@ -18,10 +14,10 @@ export class HeaderComponent implements OnInit {
   currentUser?: User;
   isLoggedIn: boolean = false;
   currentUserNotification?: Notification;
+  isHidden: boolean = false;
 
   constructor(
     private authentication: AuthenticationService,
-    public dialog: MatDialog,
     private notification: NotificationService) {
   }
 
@@ -39,28 +35,4 @@ export class HeaderComponent implements OnInit {
     this.authentication.logoutUser();
     window.location.reload();
   }
-
-  notificationPopUp() {
-    const dialogRef = this.dialog.open(NotificationPopupDialog,{
-      width: '500px',
-    })
-  }
-}
-
-@Component({
-  selector: 'notification-popup-dialog',
-  templateUrl: './notification-popup-dialog.html'
-})
-export class NotificationPopupDialog implements OnInit {
-
-  constructor(
-    public dialogRef: MatDialogRef<NotificationPopupDialog>,
-    @Inject(MAT_DIALOG_DATA) public data: MessageData
-  ) {
-  }
-
-  ngOnInit(): void {
-  }
-
-
 }
