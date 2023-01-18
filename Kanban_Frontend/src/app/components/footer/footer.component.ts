0292416ce44839988010c1d41ea7aff6c4b94ca7
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2023-01-18 15:46:38 +0530

    created isLoggedin condition in footer.component.ts

--- Kanban_Frontend/src/app/components/footer/footer.component.ts
+++ Kanban_Frontend/src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  isLoggedIn?: boolean = localStorage.getItem('token') !== null;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
-
 }
