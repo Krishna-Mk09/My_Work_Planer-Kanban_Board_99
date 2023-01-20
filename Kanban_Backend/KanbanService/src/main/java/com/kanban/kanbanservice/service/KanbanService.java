c3fe8e8ce4f5db7d85c5df1ca3c185d762f4f7ad
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2023-01-20 13:28:42 +0530

    updated java docs for kanban service

--- Kanban_Backend/KanbanService/src/main/java/com/kanban/kanbanservice/service/KanbanService.java
+++ Kanban_Backend/KanbanService/src/main/java/com/kanban/kanbanservice/service/KanbanService.java
@@ -4,10 +4,9 @@ import com.kanban.kanbanservice.domain.Kanban;
 
 import java.util.List;
 
-public interface
-KanbanService {
+public interface KanbanService {
 	/**
-	 * Get the Kanban Board by email id
+	 * This method get the Kanban Board by email id
 	 *
 	 * @param email The email if for the user
 	 * @return The Kanban Board for that user
@@ -15,7 +14,7 @@ KanbanService {
 	Kanban getKanbanByEmail(String email);
 
 	/**
-	 * Save the Kanban Board
+	 * This method save the Kanban Board
 	 *
 	 * @param kanban The Kanban Board to save
 	 * @return The saved Kanban Board
@@ -23,7 +22,7 @@ KanbanService {
 	Kanban saveKanban(Kanban kanban);
 
 	/**
-	 * Update the Kanban Board
+	 * This method updates each kanban board in all respective members
 	 *
 	 * @param kanban The Kanban Board to update
 	 * @return The updated Kanban Board
@@ -31,13 +30,27 @@ KanbanService {
 	Kanban updateKanban(Kanban kanban);
 
 	/**
-	 * Delete the Kanban Board by email id
+	 * This method delete the Kanban Board by email id
 	 *
 	 * @param email The email id for the user
 	 */
 	void deleteKanbanByEmail(String email);
 
+	/**
+	 * This method adds a member to the kanban board
+	 *
+	 * @param kanban The Kanban Board to update
+	 * @param email  The email id of the member to add
+	 * @return The updated Kanban Board
+	 */
 	Kanban addMemberToBoardByEmail(Kanban kanban, String email);
 
+	/**
+	 * This method fetched all the email ids of the members in a kanban board
+	 *
+	 * @param email     The email id of the user
+	 * @param boardName The name of the board
+	 * @return The list of email ids of the members
+	 */
 	List<String> getAllEmailsInBoard(String email, String boardName);
 }
--- Kanban_Backend/KanbanService/src/main/java/com/kanban/kanbanservice/service/KanbanServiceImpl.java
+++ Kanban_Backend/KanbanService/src/main/java/com/kanban/kanbanservice/service/KanbanServiceImpl.java
@@ -23,7 +23,7 @@ public class KanbanServiceImpl implements KanbanService {
 	}
 
 	/**
-	 * Get the Kanban Board by email id
+	 * This method get the Kanban Board by email id
 	 *
 	 * @param email The email if for the user
 	 * @return The Kanban Board for that user
@@ -34,7 +34,7 @@ public class KanbanServiceImpl implements KanbanService {
 	}
 
 	/**
-	 * Save the Kanban Board
+	 * This method save the Kanban Board
 	 *
 	 * @param kanban The Kanban Board to save
 	 * @return The saved Kanban Board
@@ -45,7 +45,7 @@ public class KanbanServiceImpl implements KanbanService {
 	}
 
 	/**
-	 * Update the Kanban Board
+	 * This method updates each kanban board in all respective members
 	 *
 	 * @param kanban The Kanban Board to update
 	 * @return The updated Kanban Board
@@ -76,7 +76,7 @@ public class KanbanServiceImpl implements KanbanService {
 	}
 
 	/**
-	 * Delete the Kanban Board by email id
+	 * This method delete the Kanban Board by email id
 	 *
 	 * @param email The email id for the user
 	 */
@@ -85,6 +85,13 @@ public class KanbanServiceImpl implements KanbanService {
 		this.KANBAN_REPOSITORY.deleteById(email);
 	}
 
+	/**
+	 * This method adds a member to the kanban board
+	 *
+	 * @param kanban The Kanban Board to update
+	 * @param email  The email id of the member to add
+	 * @return The updated Kanban Board
+	 */
 	@Override
 	public Kanban addMemberToBoardByEmail(Kanban kanban, String email) {
 		Kanban kanbanByEmail = this.KANBAN_REPOSITORY.findKanbanByEmail(email);
@@ -108,7 +115,13 @@ public class KanbanServiceImpl implements KanbanService {
 		return kanban;
 	}
 
-
+	/**
+	 * This method fetched all the email ids of the members in a kanban board
+	 *
+	 * @param email     The email id of the user
+	 * @param boardName The name of the board
+	 * @return The list of email ids of the members
+	 */
 	@Override
 	public List<String> getAllEmailsInBoard(String email, String boardName) {
 		Query query = new Query(Criteria.where("email").is(email).and("boards.boardName").is(boardName));
