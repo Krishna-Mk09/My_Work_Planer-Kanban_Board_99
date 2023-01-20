4160bb5bce9b98b49a42518189af4080ca12bdff
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2023-01-20 13:36:51 +0530

    reformatted the code

--- Kanban_Backend/KanbanService/src/main/java/com/kanban/kanbanservice/configuration/MessageConfig.java
+++ Kanban_Backend/KanbanService/src/main/java/com/kanban/kanbanservice/configuration/MessageConfig.java
@@ -1,43 +1,45 @@
 package com.kanban.kanbanservice.configuration;
 
-import org.springframework.amqp.core.*;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MessageConfig {
-	private static final String MESSAGE_EXCHANGE = "messageExchange";
-	private static final String MESSAGE_QUEUE = "messageQueue";
-	protected static final String MESSAGE_KEY = "messageKey";
+    protected static final String MESSAGE_KEY = "messageKey";
+    private static final String MESSAGE_EXCHANGE = "messageExchange";
+    private static final String MESSAGE_QUEUE = "messageQueue";
 
-	@Bean
-	public DirectExchange directExchange() {
-		return new DirectExchange(MESSAGE_EXCHANGE);
-	}
+    @Bean
+    public DirectExchange directExchange() {
+        return new DirectExchange(MESSAGE_EXCHANGE);
+    }
 
-	@Bean
-	public Queue registerQueue() {
-		return new Queue(MESSAGE_QUEUE, true);
-	}
+    @Bean
+    public Queue registerQueue() {
+        return new Queue(MESSAGE_QUEUE, true);
+    }
 
-	@Bean
-	public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
-		return new Jackson2JsonMessageConverter();
-	}
+    @Bean
+    public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 
-	@Bean
-	public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
-		RabbitTemplate rabbitTemp = new RabbitTemplate(connectionFactory);
-		rabbitTemp.setMessageConverter(producerJackson2MessageConverter());
-		return rabbitTemp;
-	}
+    @Bean
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemp = new RabbitTemplate(connectionFactory);
+        rabbitTemp.setMessageConverter(producerJackson2MessageConverter());
+        return rabbitTemp;
+    }
 
-	@Bean
-	Binding bindingUser() {
-		return BindingBuilder.bind(registerQueue()).to(directExchange()).with(MESSAGE_KEY);
-	}
+    @Bean
+    Binding bindingUser() {
+        return BindingBuilder.bind(registerQueue()).to(directExchange()).with(MESSAGE_KEY);
+    }
 }
