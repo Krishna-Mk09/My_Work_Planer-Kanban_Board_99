ab1ac749a0aef1dd6ac4f8fc50bda73384c2632b
M Vamshi Krishna <112238153+Krishna-Mk09@users.noreply.github.com>
2026-03-05 11:32:07 +0530

    Remove author comments from UserController
    Removed author comments from UserController.java

--- Kanban_Backend/UserService/src/main/java/com/kanban/userservice/controller/UserController.java
+++ Kanban_Backend/UserService/src/main/java/com/kanban/userservice/controller/UserController.java
@@ -1,10 +1,4 @@
-/*
- * Author Name: Aditya Chaurasia
- * Date: 30-12-2022
- * Created With: IntelliJ IDEA Ultimate
- * Profile: github.com/ChaurasiaAditya
- * Website: ChaurasiaAditya.in
- */
+
 package com.kanban.userservice.controller;
 
 import com.kanban.userservice.domain.User;
